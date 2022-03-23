60d0eacf5be52b05e47b43530a370f4ee9d5eb68
JustinMitchell <mitchelljm04@gmail.com>
2022-03-22 21:26:08 -0400

    commit tree 6d061a5fd0a96e844313bd2d8a7ac5a883520c44

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+      return string.charAt(0).toUpperCase()+ string.slice(1);
+  }
+  static sanitize(string){
+    if(/[^A-Za-z0-9-']+/g){
+     return string.replace(/[^A-Za-z0-9-']+/g) 
+    }
+    else{
+      return ""
+    }
+    
+
+  }
+  static titleize(string){
+   let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+   let result = [];
+   let words = string.split("")
+   for(let i = 0; i < words.length; i++){
+    if (i === 0) {
+      result.push(this.capitalize(words[i]))
+    } else {
+      if (exceptions.includes(words[i])) {
+      result.push(words[i])
+    } else {
+      result.push(this.capitalize(words[i]))
+  }}
+   }
+    return result.join("");
+  }
+  
 }
+// words[i][0].toUpperCase() + words.slice(1);
+console.log(Formatter.titleize("the dog walks"))
